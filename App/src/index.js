6d20b325b614edfa115527ae4ad009bd71be47c7
Tomiwa Ibiwoye <tomiwa@teraboxx.com>
2021-11-24 18:19:53 +0100

    Connected Redux

--- App/src/index.js
+++ App/src/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
 import Box from "@mui/material/Box";
 import { ThemeProvider } from "@mui/material/styles";
 import { createTheme } from "@mui/material/styles";
@@ -7,31 +8,37 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import {
-  AboutUs,
-  Disclaimer,
-  Home,
-  PrivacyPolicy,
-  Subscribe,
+    AboutUs,
+    Disclaimer,
+    Home,
+    PrivacyPolicy,
+    Subscribe,
 } from "./features";
+
+import configureStore from "./store";
 
 const mdTheme = createTheme();
 
+const store = configureStore()
+
 ReactDOM.render(
-  <ThemeProvider theme={mdTheme}>
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="subscribe" element={<Subscribe />} />
-            <Route path="aboutus" element={<AboutUs />} />
-            <Route path="privacypolicy" element={<PrivacyPolicy />} />
-            <Route path="disclaimer" element={<Disclaimer />} />
-            <Route path="/" element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Box>
-  </ThemeProvider>,
-  document.querySelector("#root")
+    <Provider store={store}>
+        <ThemeProvider theme={mdTheme}>
+            <Box sx={{ display: "flex" }}>
+                <CssBaseline />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<App />}>
+                            <Route path="subscribe" element={<Subscribe />} />
+                            <Route path="aboutus" element={<AboutUs />} />
+                            <Route path="privacypolicy" element={<PrivacyPolicy />} />
+                            <Route path="disclaimer" element={<Disclaimer />} />
+                            <Route path="/" element={<Home />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </Box>
+        </ThemeProvider>
+    </Provider>,
+    document.querySelector("#root")
 );
