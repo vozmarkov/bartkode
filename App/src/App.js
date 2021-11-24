7e389439598cdbf0c7f792d8d6f8223a54188657
DESKTOP-IEE2V5I\admin <mvoznyuk@partners.org>
2021-11-25 00:50:14 +0200

    loading spinner start

--- App/src/App.js
+++ App/src/App.js
@@ -112,10 +112,6 @@ function AppContent() {
       <Box
         component="main"
         sx={{
-          backgroundColor: (theme) =>
-            theme.palette.mode === "light"
-              ? theme.palette.grey[100]
-              : theme.palette.grey[900],
           flexGrow: 1,
           height: "100vh",
           overflow: "auto",
--- App/src/features/home/Home.jsx
+++ App/src/features/home/Home.jsx
@@ -1,11 +1,17 @@
-import React, { useEffect } from "react";
-// import { fetchGiftPosts } from "../../store/posts/posts";
-// import { useDispatch, useSelector } from "react-redux";
+import * as React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Stack from "@mui/material/Stack";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const loadiong = useSelector((state) => state.loadiong);
   // const [giftPosts, setGiftPosts] = useState([]);
 
-  return <div>THis is HOME</div>;
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <CircularProgress />
+    </div>
+  );
 };
 
 export default Home;
